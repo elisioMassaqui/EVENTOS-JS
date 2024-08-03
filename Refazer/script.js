06e80967e314f2a18235798085ae709c15e54195
Mr.Robot <elisiomassaqui8@gmail.com>
2024-08-03 20:28:19 +0100

    pratique

---
+++ Refazer/script.js
@@ -0,0 +1,6 @@
+function pass(params) {
+    const textInput = document.getElementById('textInput')
+    const textArea = document.getElementById('textArea')
+    textArea.value = textInput.value
+    console.log('funfandooo')
+}
